26a88f313784e0450312dde5467da388bfeb3ca5
palaciorecords01-glitch <palaciorecords01@gmail.com>
2025-10-03 14:56:58 +0100

    Add files via upload

---
+++ palaciorecords/js/script.js
@@ -0,0 +1,5 @@
+const toggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+toggle.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
